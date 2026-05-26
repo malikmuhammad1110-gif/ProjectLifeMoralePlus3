8c30de2f6fdd76b87027f1ada0c8f387cb1a6cc1
malikmuhammad1110-gif <malik.muhammad1110@gmail.com>
2026-05-25 20:02:57 -0400

    Change default size and update logo display

--- components/LogoPLM.tsx
+++ components/LogoPLM.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 
-export default function LogoPLM({ size = 56 }: { size?: number }) {
+export default function LogoPLM({ size = 72 }: { size?: number }) {
   return (
-    <img
-      src="/plm-logo.png"
-      alt="PLM+"
-      width={size}
-      height={size}
+    <div
       style={{
         width: size,
         height: size,
-        objectFit: "contain",
-        display: "block",
+        borderRadius: 24,
+        overflow: "hidden",
+        boxShadow: "0 8px 24px rgba(0,0,0,0.18)",
+        background: "#ffffff",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
-    />
+    >
+      <img
+        src="/plm-logo2.png"
+        alt="PLM+ Logo"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          display: "block",
+        }}
+      />
+    </div>
   );
 }
